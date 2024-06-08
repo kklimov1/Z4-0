e847402b50411d8ec772be9de36a816b35b489f8
kklimov1 <kirillvlklimov@gmail.com>
2024-06-07 22:01:18 -0400

    Working version on local end

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,14 +1,16 @@
 import axios from "axios";
 import { ACCESS_TOKEN } from "./constants";
 
-const apiUrl = "/choreo-apis/djangoreactatt/backend/v1";
+//const apiUrl = "/choreo-apis/djangoreactatt/backend/v1";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL : apiUrl,
+  baseURL: import.meta.env.VITE_API_URL // ? import.meta.env.VITE_API_URL : apiUrl,
+  
 });
 
 api.interceptors.request.use(
   (config) => {
+    
     const token = localStorage.getItem(ACCESS_TOKEN);
     if (token) {
       config.headers.Authorization = `Bearer ${token}`;
